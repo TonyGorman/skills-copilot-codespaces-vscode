553dc93459d8f1e81534200aaace2a8bd4cf8d72
Tony Gorman <tony.gorman@asos.com>
2024-03-28 11:21:35 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+// add a health check endpoint to the server
+app.get('/health', (req, res) => {
+    res.send('Server is healthy');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
